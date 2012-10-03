63a02b8e6d31e45354a3d63f9003db92166911ab
Eric Schulte <eric.schulte@gmx.com>
2012-10-03 00:11:57 -0600

    failed BAIL C macro

--- src/gpg.cc
+++ src/gpg.cc
@@ -13,6 +13,11 @@
 #include <locale.h>
 #include <gpgme.h>
 
+#define BAIL(x,y) if(err = x){                                                 \
+    return Exception::Error(String::Concat(String::New(y),                     \
+                                           String::New(gpgme_strerror(err)))); \
+  }
+
 using namespace v8;
 using namespace node;
 
@@ -20,42 +25,31 @@ using namespace node;
 // So more than one operation can run at a time.
 gpgme_ctx_t ctx;
 
-void bail(gpgme_error_t err, const char * msg){
-  if(err){
-    printf("%s: [error] %s\n", msg, gpgme_strerror(err));
-    exit(err); } }
-
-void init(){
-  printf("initializing...\n");
-  setlocale (LC_ALL, "");
-  gpgme_check_version (NULL);
-  gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
-  #ifdef LC_MESSAGES
-  gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
-  #endif
-  bail(gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP),
-       "Initializing the engine");
-}
-
 void str_to_data(gpgme_data_t *data, const char* string){
-  bail(gpgme_data_new_from_mem(data, string, strlen(string), 1),
-       "creating a data buffer from memory"); }
+  gpgme_data_new_from_mem(data, string, strlen(string), 1); }
 
 Handle<Value>Verify(const Arguments& args) {
   HandleScope scope;
 
+  gpgme_error_t err;
   gpgme_ctx_t ctx;
   gpgme_data_t SIG, DATA;
   gpgme_verify_result_t result;
   gpgme_signature_t sig;
 
   /* setup */
-  printf("initializing...\n");
-  init();
+  setlocale (LC_ALL, "");
+  gpgme_check_version (NULL);
+  gpgme_set_locale (NULL, LC_CTYPE, setlocale (LC_CTYPE, NULL));
+  #ifdef LC_MESSAGES
+  gpgme_set_locale (NULL, LC_MESSAGES, setlocale (LC_MESSAGES, NULL));
+  #endif
+
+  BAIL(gpgme_engine_check_version(GPGME_PROTOCOL_OpenPGP),
+       "engine initialization error:");
 
   /* create context */
-  printf("creating a context...\n");
-  bail(gpgme_new(&ctx), "Creating a context");
+  BAIL(gpgme_new(&ctx), "context creation error:")
 
   /* parse arguments */
   if (args.Length() != 2)
@@ -66,28 +60,19 @@ Handle<Value>Verify(const Arguments& args) {
     return ThrowException(Exception::TypeError(
       String::New("First argument is a string (signature)")));
   String::Utf8Value signature(args[0]->ToString());
-  printf("converting first argument to in-memory data...\n");
   str_to_data(&SIG, *signature);
 
   if (!args[1]->IsString())
     return ThrowException(Exception::TypeError(
       String::New("Second argument is a string (data)")));
   String::Utf8Value data(args[1]->ToString());
-  printf("converting second argument to in-memory data...\n");
   str_to_data(&DATA, *data);
 
-  printf("verifying signature...\n");
-  bail(gpgme_op_verify(ctx, SIG, DATA, NULL),
-       "verifying");
+  BAIL(gpgme_op_verify(ctx, SIG, DATA, NULL), "verification error:");
 
-  printf("getting result...\n");
   result = gpgme_op_verify_result(ctx);
   sig = result->signatures;
 
-  printf("checking status...\n");
-  printf("status=%d\n", sig->status);
-
-  printf("returning...\n");
   if(sig->status == GPG_ERR_NO_ERROR) return True();
   else                                return False();
 }
--- test.js
+++ test.js
@@ -2,11 +2,7 @@ var gpg = require('./build/Release/gpg');
 
 console.log('verifying');
 
-var signature = "-----BEGIN PGP SIGNATURE-----\nVersion: GnuPG v2.0.19 (GNU/Linux)\n\niQEcBAABAgAGBQJQa3thAAoJEDwbhYFhTKBdrwsH/jK7rDbodKgZ1CNdKOjOmsWD\ntUC+brptZ+y78AwUPKusIv2t3HBcsecxmn8+dGXiXPLZQJ7cIDtAf8gf78/zHzHh\nnshii5qTYEeMGADTWlMbK9Kdi19t6N3GD6VHm1h2GLuOUi+vRx6WyZ8rPjkcoeR1\n6MGUHBuxPxMct0N+wKmzM+H+uabL3ysiYAyNpO1Q8m//aw/bqtvsydVsfjFAuzBW\nnOswdATDkDkQIl+maVsbrW0jJO6Dqp9RxjdKygObeIY7r8xWiqGdto2oV+4FFDQO\nOyU4jzhtDpGa+jeU8BGRnveiWQgn3Q0NBQnQmrt4rrbKcZAWbsEsfgkCFZhzqYI=\n=FGNI\n-----END PGP SIGNATURE-----";
-
-var data = "patton\n";
-
-var verrified = gpg.verify(signature, data);
+var verrified = gpg.verify('', '');
 
 if(verrified){
   console.log("success");
